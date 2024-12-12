327bd74a07e5dbd93cde0b7670ac35214301a5ee
David Kyndl <david.kyndl@student.gyarab.cz>
2024-12-12 09:57:01 +0100

    bgngrth

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ def fibonacci(n):
         print(fibonacci(250))
 
 def is_prime(n):
-    if not isinstance(n, int) or n < 2:
-        raise ValueError("Input must be an integer greater than or equal to 2.")
+    if not isinstance(n, int) or n <= 1:
+        raise ValueError("Input must be an integer greater than 1.")
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
@@ -21,3 +21,6 @@ def is_prime(n):
 
     if __name__ == "__main__":
         print(is_prime(97))
+
+print(fibonacci(255))
+print(is_prime(5))
