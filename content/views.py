c1a9ec52518cf7b2f701ad196d855a92343df016
David Kyndl <david.kyndl@student.gyarab.cz>
2025-01-30 12:07:44 +0100

    nhtrhgt

--- content/views.py
+++ content/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import requests
+
+data = [
+    {
+        'title': 'Python Django Tutorial',
+        'url': 'https://www.youtube.com/watch?v=F5mRW0jo-U4',
+    },
+    {
+        'title': 'Bohemians 1905',
+        'url': 'https://www.bohemians.cz/',
+
+    }
+]
+
+def homepage(request):
+    ret = '''
+    <!DOCTYPE html>
+    <html>
+    <head></head>
+    <body>
+    <ul>
+    '''
+
+    for i in data:
+        ret += f"""
+        <li>
+            <a href="{i['url']}">{i['title']}</a>
+        </li>"""
+
+    ret += "</ul></body></html>"
+    return HttpResponse(ret)
