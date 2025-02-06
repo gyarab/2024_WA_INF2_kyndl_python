5b7275294271754a87fb2ebf1391c8cd2bdf2fe7
David Kyndl <david.kyndl@student.gyarab.cz>
2025-02-06 09:09:16 +0100

    hbttr

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from content.views import homepage
+from content.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
