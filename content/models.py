0bfec6f62c517df24d77cd824d3e825f7be24dd1
David Kyndl <david.kyndl@student.gyarab.cz>
2025-03-06 10:06:06 +0100

    skoles

--- content/models.py
+++ content/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Bohemka(models.Model):
+    název = models.CharField(max_length=100)
+    cena = models.DecimalField(max_digits=10, decimal_places=2)
+    obrázek = models.URLField()
+    odkaz = models.URLField()
+    
